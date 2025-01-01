cfff4749a9b9589fea3f070f6920a8c27ea9ac2e
Eliasagidew <bossmamboss01@gmail.com>
2025-01-01 10:50:21 +0300

    first commit

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -3,7 +3,7 @@ import './Item.css'
 const Item = (props) => {
   return (
     <div className='item'>
-      <img src={props.imge} alt="" />
+      <img src={props.image} alt="" />
       <p>{props.name}</p>
       <div className='item_prices'>
         <div className='item-price-new'>
